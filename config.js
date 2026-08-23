3e41057238a33200698c78f145571ee3654921ca
petruthagima1-droid <petruthagima1@gmail.com>
2026-08-23 16:15:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // Client ID obținut din Google Cloud Console (tip "Web application")
   // Exemplu: "123456789-abc123xyz.apps.googleusercontent.com"
-  GOOGLE_CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "790957472122-1elcatkihnauk01i2lnl59pv6d9rl092.apps.googleusercontent.com",
 
   // ID-ul spreadsheet-ului (din URL, între /d/ și /edit)
   SPREADSHEET_ID: "1lRHy8kdx597HZa4U60XzMZ3k44F-sY_B8n205He6X2Y",
